7e2afcbd7f64777b5fc7d4eef9a4a7cc2fe01245
Marcos Oliveira <61069282+MarcosOlv@users.noreply.github.com>
2024-09-19 03:07:06 -0300

    add explicacao

--- Nivel 1 Basico Java/Ex 001 Ola mundo/app.js
+++ Nivel 1 Basico Java/Ex 001 Ola mundo/app.js
@@ -1,5 +1,10 @@
 let Mundo = ('Ola mundo');
 alert(Mundo);
 
+// EXPLICAÇÃO
+
+// let mundo define uma variavel que tem o texto 
+// o alert(mundo) servira para exibir um pop-up na tela dizendo o ola mundo
+
 
 /// Retirado da lista de Exercicios - https://gustavoguanabara.github.io/javascript/exercicios/
